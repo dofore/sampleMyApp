1c7ea5752dd46a2fce801e94b77eb7e84ae46ffb
dofore <katwoma81@gmail.com>
2018-02-03 18:52:48 +0900

    initial commit

---
+++ app/src/androidTest/java/com/android/palladium/omikujiapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.android.palladium.omikujiapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
